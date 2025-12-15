574dffeddfe9c2ddb41c51d805553bdeb2307215
kalebemattos <kalebe.mattos@live.com>
2025-12-15 14:09:53 -0300

    cutt

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
@@ -31,7 +31,7 @@ const App = () => (
       <AuthProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <HashRouter>
           <Routes>
             {/* Site comum */}
             <Route path="/" element={<Index />} />
@@ -39,11 +39,11 @@ const App = () => (
             {/* Login Admin */}
             <Route path="/admin/login" element={<AdminLogin />} />
 
-            {/* Rotas de recuperação de senha */}
+            {/* Recuperação de senha */}
             <Route path="/admin/forgot" element={<ForgotPassword />} />
             <Route path="/admin/reset" element={<ResetPassword />} />
 
-            {/* Área admin (protegida no AdminLayout) */}
+            {/* Área admin protegida */}
             <Route path="/admin" element={<AdminLayout />}>
               <Route index element={<AdminDashboard />} />
               <Route path="pdv" element={<AdminPDV />} />
@@ -59,7 +59,7 @@ const App = () => (
             {/* Not found */}
             <Route path="*" element={<NotFound />} />
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
       </AuthProvider>
     </TooltipProvider>
   </QueryClientProvider>
