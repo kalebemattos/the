7600823848afdd6302acf859f638f447d63e5e77
kalebemattos <kalebe.mattos@live.com>
2025-12-18 16:11:18 -0300

    ka

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -1,7 +1,13 @@
 import { createClient } from "@supabase/supabase-js";
 
-// Use suas variáveis de ambiente definidas no .env
 export const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY
+  import.meta.env.VITE_SUPABASE_URL!,
+  import.meta.env.VITE_SUPABASE_ANON_KEY!,
+  {
+    auth: {
+      persistSession: true,
+      autoRefreshToken: true,
+      detectSessionInUrl: true,
+    },
+  }
 );
